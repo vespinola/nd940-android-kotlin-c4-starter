e8f89a3d373486290c85b6915dd036bfd2beb8a4
Vladimir Espinola Lezcano <vladimir.espinola@gmail.com>
2021-04-02 11:15:05 -0400

    finished detail activity

--- starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -1,10 +1,17 @@
 package com.udacity.project4.locationreminders
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
@@ -12,8 +19,9 @@ import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 /**
  * Activity that displays the reminder details after the user clicks on the notification
  */
-class ReminderDescriptionActivity : AppCompatActivity() {
-
+class ReminderDescriptionActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+    private lateinit var reminderDataItem: ReminderDataItem
     companion object {
         private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
 
@@ -32,6 +40,32 @@ class ReminderDescriptionActivity : AppCompatActivity() {
             this,
             R.layout.activity_reminder_description
         )
+
+        reminderDataItem = intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem
 //        TODO: Add the implementation of the reminder details
+        binding.reminderDataItem = reminderDataItem
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap?.let {
+            map = googleMap
+
+            val latLng = LatLng(reminderDataItem.latitude!!, reminderDataItem.longitude!!)
+
+            map.addMarker(
+                    MarkerOptions()
+                            .position(latLng)
+                            .title(reminderDataItem.title)
+            )
+
+            val zoomLevel = 20f
+            val homeLatLng = LatLng(latLng.latitude, latLng.longitude)
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        }
     }
 }
